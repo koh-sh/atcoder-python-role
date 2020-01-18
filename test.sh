79a97933807dd0383918dfa738f65070f4ec9941
koh-sh <koh.shinoshi@gmail.com>
2020-01-18 14:01:02 +0900

    fix indent and echo

--- test.sh
+++ test.sh
@@ -11,13 +11,17 @@ cd "$contest"/"$task"
 
 for i in $(seq 1 "$samples")
 do
-	  res=$(python main.py < in_"${i}".txt)
-	  ans=$(cat out_"${i}".txt)
-	  if [ "$res" == "$ans" ]; then
-	  	  echo "sample${i} AC"
-	  else
-	  	  echo "sample${i} WA"
+    res=$(python main.py < in_"${i}".txt)
+    ans=$(cat out_"${i}".txt)
+    if [ "$res" == "$ans" ]; then
+        echo "=============="
+        echo "sample${i} AC"
+    else
+        echo "=============="
+        echo "sample${i} WA"
+        echo -e "-----\nOutput:"
         echo "$res"
+        echo -e "-----\nExpected:"
         echo "$ans"
-	  fi
+    fi
 done
